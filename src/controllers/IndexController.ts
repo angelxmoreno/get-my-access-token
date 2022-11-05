6d2a3e588e0da7430c342e9b1a51db72df242976
Angel S. Moreno <angel.moreno@dotdashmdp.com>
2022-11-04 22:23:35 -0400

    passing ModularAuthCollection to FE

--- src/controllers/IndexController.ts
+++ src/controllers/IndexController.ts
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Get, JsonController, Render } from 'routing-controllers';
+import ModularAuthCollection from '@services/modular-auth/ModularAuthCollection';
 
 @JsonController('/')
 export class IndexController {
     @Get('')
     @Render('index')
-    index() {}
+    index() {
+        return {
+            ModularAuthCollection,
+        };
+    }
 
     @Get('about')
     @Render('about')
